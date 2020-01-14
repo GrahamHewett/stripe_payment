56d090dba3f813aa37235a219ff4c52f496c025e
Graham Hewett <hewettgraham@gmail.com>
2020-01-14 22:48:00 +0000

    minimum config for datapicker

--- src/DatePicker.js
+++ src/DatePicker.js
@@ -1,141 +1,142 @@
 import React from 'react'
 import CarbonDatePicker from 'react-carbon-datepicker'
  
-let configTheme = {
-	squareEdge: false,
-	background: '#fff',
-	dateHeaderColor: '#777',
-	button: {
-		background: '#fff',
-		color: '#999',
-		hover: {
-			background: '#f9f9f9',
-			color: '#555',
-		}
-	},
-	input: {
-		background: '#f4f4f4',
-		color: '#555',
-	},
-	year: {
-		selector: {
-			background: '#fff',
-			pointerColor: 'rgba(0,0,0,0.03)',
-			listItem: {
-				background: 'transparent',
-				color: '#999',
-				hover: {
-					background: 'transparent',
-					color: '#333',
-				},
-				active: {
-					background: 'transparent',
-					color: '#333',
-					hover: {
-						background: 'transparent',
-						color: '#222',
-					},
-				}
-			},
-			button: {
-				background: '#fff',
-				color: '#999',
-				hover: {
-					background: '#f9f9f9',
-					color: '#555',
-				}
-			},
-		},
-		button: {
-			background: '#fff',
-			color: '#555',
-			hover: {
-				background: '#f9f9f9',
-				color: '#222',
-			}
-		},
-	},
-	month: {
-		button: {
-			background: '#fff',
-			color: '#555',
-			hover: {
-				background: '#f9f9f9',
-				color: '#222',
-			}
-		},
-		selector: {
-			background: 'rgba(255,255,255,0.95)',
-			button: {
-				background: 'transparent',
-				color: '#777',
-				hover: {
-					background: '#f5f5f5',
-					color: '#444',
-				},
-				active: {
-					background: '#c1e2c0',
-					color: '#555',
-					hover: {
-						background: '#b0d8af',
-						color: '#444',
-					},
-				}
-			}
-		}
-	},
-	date: {
-		background: 'transparent',
-		color: '#555',
-		hover: {
-			background: '#f9f9f9',
-			color: '#333',
-		},
-		active: { 
-			background: '#ed1f4f',
-			color: '#fff',
-			hover: {
-				background: '#ed1f4f',
-				color: '#fff',
-			}
-		},
-		highlight: {
-			background: '#fde1e7',
-			color: '#333',
-			hover: {
-				background: '#fde1e7',
-				color: '#333',
-			}
-		},
-		disabled: {
-			opacity: 0.4,
-			color: '#555',
-			background: 'transparent'
-		}
-	},
-	reminder: {
-		headerColor: '#666',
-		descriptionColor: '#999' 
-	}
-}
-
 const configObj = {
-	alwaysShow:	true,
-	showCalendar:	true,
+	alwaysShow:	false,
+	showCalendar:	false,
 	themePreset: 'dark',
 	reminders:	[
-		{note:	'I am a reminder', color: 'green', timestamp: 0,}, 
-		{note:	'I am a 2nd reminder', color: 'blue', timestamp: 1,}
+		//query the timestamp value?
+		{note:	'I am a reminder', color: 'green', timestamp: '2020-02-02'}, 
+		{note:	'I am a 2nd reminder', color: 'blue', timestamp: 1}
 	],
-	theme: configTheme,
-}
-
-const handleChange = () => {
-
+	theme: '', //configTheme object can be the value here
 }
 
 export default function DatePicker() {
     return (
-      <CarbonDatePicker onChange={handleChange} config={configObj}/>
+      <CarbonDatePicker config={configObj}/>
     )
 }
+
+// const handleChange = () => {
+
+// }
+
+// let configTheme = {
+// 	squareEdge: false,
+// 	background: '#fff',
+// 	dateHeaderColor: '#777',
+// 	button: {
+// 		background: '#fff',
+// 		color: '#999',
+// 		hover: {
+// 			background: '#f9f9f9',
+// 			color: '#555',
+// 		}
+// 	},
+// 	input: {
+// 		background: '#f4f4f4',
+// 		color: '#555',
+// 	},
+// 	year: {
+// 		selector: {
+// 			background: '#fff',
+// 			pointerColor: 'rgba(0,0,0,0.03)',
+// 			listItem: {
+// 				background: 'transparent',
+// 				color: '#999',
+// 				hover: {
+// 					background: 'transparent',
+// 					color: '#333',
+// 				},
+// 				active: {
+// 					background: 'transparent',
+// 					color: '#333',
+// 					hover: {
+// 						background: 'transparent',
+// 						color: '#222',
+// 					},
+// 				}
+// 			},
+// 			button: {
+// 				background: '#fff',
+// 				color: '#999',
+// 				hover: {
+// 					background: '#f9f9f9',
+// 					color: '#555',
+// 				}
+// 			},
+// 		},
+// 		button: {
+// 			background: '#fff',
+// 			color: '#555',
+// 			hover: {
+// 				background: '#f9f9f9',
+// 				color: '#222',
+// 			}
+// 		},
+// 	},
+// 	month: {
+// 		button: {
+// 			background: '#fff',
+// 			color: '#555',
+// 			hover: {
+// 				background: '#f9f9f9',
+// 				color: '#222',
+// 			}
+// 		},
+// 		selector: {
+// 			background: 'rgba(255,255,255,0.95)',
+// 			button: {
+// 				background: 'transparent',
+// 				color: '#777',
+// 				hover: {
+// 					background: '#f5f5f5',
+// 					color: '#444',
+// 				},
+// 				active: {
+// 					background: '#c1e2c0',
+// 					color: '#555',
+// 					hover: {
+// 						background: '#b0d8af',
+// 						color: '#444',
+// 					},
+// 				}
+// 			}
+// 		}
+// 	},
+// 	date: {
+// 		background: 'transparent',
+// 		color: '#555',
+// 		hover: {
+// 			background: '#f9f9f9',
+// 			color: '#333',
+// 		},
+// 		active: { 
+// 			background: '#ed1f4f',
+// 			color: '#fff',
+// 			hover: {
+// 				background: '#ed1f4f',
+// 				color: '#fff',
+// 			}
+// 		},
+// 		highlight: {
+// 			background: '#fde1e7',
+// 			color: '#333',
+// 			hover: {
+// 				background: '#fde1e7',
+// 				color: '#333',
+// 			}
+// 		},
+// 		disabled: {
+// 			opacity: 0.4,
+// 			color: '#555',
+// 			background: 'transparent'
+// 		}
+// 	},
+// 	reminder: {
+// 		headerColor: '#666',
+// 		descriptionColor: '#999' 
+// 	}
+// }
